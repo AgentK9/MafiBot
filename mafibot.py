4489c5656a1cabec83e0cad53ce5978c129c6bdf
Keegan Birt <kbirt@birtdata.com>
2020-05-17 11:04:00 -0400

    various stuff to class, started voting

--- mafibot.py
+++ mafibot.py
@@ -1,25 +1,143 @@
 import discord
 from random import shuffle
-from time import sleep
+from asyncio import sleep
 
 # TODO: add config/persistent file
 token = "your token here"
-gamestate = None
 startchannel = None
 
 mafiachannel = None
 copchannel = None
 generalchannel = None
 
-nominations = {}
-
-maf = None
-
 
 class Mafia:
-    def __init__(self, playe: list):
-        self.players = playe
+    def __init__(self):
+        self.players = []
         self.playerRoles = {}
+        self.gamestate = "initialized"
+        self.nominations = {}
+        self.responded = 0
+
+    async def increment_gamestate(self):
+        if self.gamestate == "initialized":
+            await startchannel.send('@Notifications:\n'
+                                    'Starting game.\n '
+                                    'Everyone wishing to participate, '
+                                    'please react with a :thumbsup: to this message.')
+            self.gamestate = "waiting for players"
+        elif self.gamestate == "waiting for players":
+            await startchannel.send('Closing sign-ups.')
+            await sleep(5)
+            await startchannel.send('Sign-ups closed. Sending out roles via DMs.')
+
+            self.gamestate = "setting up"
+            counter = 0
+            # get signupMessage
+            signupMessage = None
+            async for message in startchannel.history(limit=50):
+                if message.author == client.user:
+                    counter += 1
+                if counter == 3:
+                    signupMessage = message
+                break
+
+            # get all users who reacted to the signup message
+            for reaction in signupMessage.reactions:
+                if reaction.emoji == '👍':
+                    self.players = await reaction.users().flatten()
+
+            # DM and set roles for mafia, cops, villagers
+            userRoles = self.assignroles()
+            for role in userRoles.keys():
+                users = userRoles[role]
+                for user in users:
+                    if not user.dm_channel:
+                        await user.create_dm()
+
+                    user.dm_channel.send(role)
+                    if role == "Mafia":
+                        await mafiachannel.set_permissions(user, read_messages=True,
+                                                           send_messages=False)
+                    elif role == "Cop":
+                        await copchannel.set_permissions(user, read_messages=True,
+                                                         send_messages=False)
+                    else:
+                        await mafiachannel.set_permissions(user, read_messages=False,
+                                                           send_messages=False)
+                        await copchannel.set_permissions(user, read_messages=False,
+                                                         send_messages=False)
+
+                    await generalchannel.set_permissions(user, read_messages=True,
+                                                         send_messages=False)
+
+            # @ all the mafia and tell them that they're mafia
+            for user in userRoles["Mafia"]:
+                await mafiachannel.send('@' + user.display_name)
+            await mafiachannel.send('You are mafia')
+
+            # @ all the cops and tell them that they're cops
+            for user in userRoles["Cop"]:
+                await copchannel.send('@' + user.display_name)
+            await copchannel.send('You are cops')
+            self.gamestate = "set up"
+        elif self.gamestate == "set up":
+            await self.play()
+
+    async def play(self):
+        gameDone = False
+        while not gameDone:
+            # DAY
+            # give chat perms
+            for user in maf.players:
+                await generalchannel.set_permissions(user, read_messages=True,
+                                                     send_messages=True)
+            await sleep(300)
+            # take chat perms
+            for user in maf.players:
+                await generalchannel.set_permissions(user, read_messages=True,
+                                                     send_messages=False)
+            self.gamestate = "day nominations"
+            # ask for nominations
+            for user in maf.players:
+                await generalchannel.set_permissions(user, read_messages=True,
+                                                     send_messages=True)
+                await generalchannel.send('@' + user.display_name + ', who do you nominate? '
+                                                                    '(nominate with !nom @{person})')
+
+                for i in range(15):
+                    old_responded = self.responded
+                    await sleep(1)
+                    if self.responded > old_responded:
+                        break
+                    if i >= 14:
+                        await generalchannel.send('Assuming that no nomination is an abstention, @' +
+                                                  user.display_name + '.')
+                await generalchannel.set_permissions(user, read_messages=True, send_messages=False)
+
+            self.responded = 0
+            # vote
+            self.gamestate = "day voting"
+            for user in maf.players:
+                await generalchannel.set_permissions(user, read_messages=True,
+                                                     send_messages=True)
+                await generalchannel.send('@' + user.display_name + ', who do you vote for? '
+                                                                    '(nominate with !v @{person})')
+
+                for i in range(15):
+                    old_responded = self.responded
+                    await sleep(1)
+                    if self.responded > old_responded:
+                        break
+                    if i >= 14:
+                        await generalchannel.send('Assuming that no vote is an abstention, @' +
+                                                  user.display_name + '.')
+                await generalchannel.set_permissions(user, read_messages=True, send_messages=False)
+            # get voting results
+            dead_user = max(self.nominations, key=lambda key: self.nominations[key])
+            self.death(dead_user)
+
+
 
     def __getroles(self):
         numMaf = round(len(self.players)/4)
@@ -43,12 +161,12 @@ class Mafia:
 
         return self.playerRoles
 
-    def death(self, playerID):
+    def death(self, player):
         gameDone = False
         msg = ""  # placeholder
         dead = None
         for player in self.players:
-            if player.id == playerID:
+            if player == player:
                 dead = player
         self.players.remove(dead)
         deadRole = None
@@ -90,6 +208,7 @@ class Mafia:
 
 
 client = discord.Client()
+maf = Mafia()
 
 
 @client.event
@@ -98,7 +217,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    global gamestate, startchannel, mafiachannel, copchannel, generalchannel
+    global gamestate, startchannel, mafiachannel, copchannel, generalchannel, maf
     if message.author == client.user:
         return
 
@@ -113,114 +232,30 @@ async def on_message(message):
             if not generalchannel:
                 await message.channel.send('Please set the general channel with the command !setgeneral')
         # start the game
-        if not gamestate:
+        if maf.gamestate == "initialized":
             startchannel = message.channel
-            await startchannel.send('@Notifications:\n'
-                                       'Starting game.\n '
-                                       'Everyone wishing to participate, '
-                                       'please react with a :thumbsup: to this message.')
-            gamestate = "waitingForPlayers"
-            print(gamestate)
-        elif gamestate == "waitingForPlayers":
-            await startchannel.send('Closing sign-ups.')
-            sleep(5)
-            await startchannel.send('Sign-ups closed. Sending out roles via DMs.')
-            counter = 0
-            gamestate = "setting up"
-            print(gamestate)
-
-            # get signupMessage
-            signupMessage = None
-            async for message in startchannel.history(limit=50):
-                if message.author == client.user:
-                    counter += 1
-                if counter == 3:
-                    signupMessage = message
-                break
-
-            # get all users who reacted to the signup message
-            for reaction in signupMessage.reactions:
-                if reaction.emoji == '👍':
-                    maf = Mafia(await reaction.users().flatten())
-
-            # DM and set roles for mafia, cops, villagers
-            userRoles = maf.assignroles()
-            for role in userRoles.keys():
-                users = userRoles[role]
-                for user in users:
-                    if not user.dm_channel:
-                        await user.create_dm()
-
-                    user.dm_channel.send(role)
-                    if role == "Mafia":
-                        await mafiachannel.set_permissions(user, read_messages=True,
-                                                                 send_messages=False)
-                    elif role == "Cop":
-                        await copchannel.set_permissions(user, read_messages=True,
-                                                               send_messages=False)
-                    else:
-                        await mafiachannel.set_permissions(user, read_messages=False,
-                                                                 send_messages=False)
-                        await copchannel.set_permissions(user, read_messages=False,
-                                                               send_messages=False)
-
-                    await generalchannel.set_permissions(user, read_messages=True,
-                                                               send_messages=False)
-
-            # @ all the mafia and tell them that they're mafia
-            for user in userRoles["Mafia"]:
-                await mafiachannel.send('@' + user.display_name)
-            await mafiachannel.send('You are mafia')
-
-            # @ all the cops and tell them that they're cops
-            for user in userRoles["Cop"]:
-                await copchannel.send('@' + user.display_name)
-            await copchannel.send('You are cops')
-
-            gamestate = "playing"
-            print(gamestate)
-            gameDone = False
-            while not gameDone:
-                gamestate = "playing-night"
-                gamestate = "playing-night-mafia"
-                # mafia
-                #  - discussion
-                #  - voting
-                # cops
-                #  - voting
-                gamestate = "playing-day"
-                gamestate = "playing-day-discussion"
-                gamestate = "playing-day-discussion-give chat perms"
-                for user in maf.players:
-                    await generalchannel.set_permissions(user, read_messages=True,
-                                                         send_messages=True)
-                sleep(300)
-                gamestate = "playing-day-discussion-take chat perms"
-                for user in maf.players:
-                    await generalchannel.set_permissions(user, read_messages=True,
-                                                         send_messages=False)
-                gamestate = "playing-day-discussion-nominations"
-                for user in maf.players:
-                    await generalchannel.set_permissions(user, read_messages=True,
-                                                         send_messages=True)
-                    await generalchannel.send('@' + user.display_name + ', who do you nominate? '
-                                                                        '(nominate with !nom @{person})')
-                gamestate = "playing-day-discussion-voting"
+            maf.increment_gamestate()
+        elif gamestate == "waiting for players":
+            maf.increment_gamestate()
 
     # nominations
     if message.channel == generalchannel and message.content.startswith('!nom') and \
-            gamestate == "playing-day-discussion-nominations":
+            gamestate == "day nominations":
         if len(message.mentions) != 1:
             await generalchannel.send('You can only nominate one person. Please try again.')
         else:
-            nominations[str(message.mentions[0].name)] = 0
+            maf.nominations[str(message.mentions[0].name)] = 0
+            maf.responded += 1
     # voting
     if message.content.startswith('!v'):
-        if message.channel == generalchannel and gamestate == "playing-day-discussion-voting":
+        if message.channel == generalchannel and gamestate == "day voting":
             if len(message.mentions) != 1:
                 await message.channel.send('You can only vote for one person. Please try again.')
             else:
-                nominations[str(message.mentions[0].name)] += 1
+                try:
+                    maf.nominations[str(message.mentions[0].name)] += 1
+                except KeyError:
+                    await message.channel.send('You cannot vote for someone who was not nominated. Please try again.')
         elif message.channel == mafiachannel and gamestate == "playing-night-mafia-voting":
             if len(message.mentions) != 1:
                 await message.channel.send('You can only vote for one person. Please try again.')
